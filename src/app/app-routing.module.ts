6b314a6d66d826b0238f4b8cac5dc78cfa6d0092
Alhazen4 <cybernonk404@gmail.com>
2023-08-11 09:36:00 +0700

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { WorkComponent } from './components/work/work.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent, title: 'Ardi Azizi | FrontEnd & UI UX'},
+  {path: 'work', component: WorkComponent, title: 'Ardi Azizi | Work'},
+  {path: 'about', component: AboutComponent, title: 'Ardi Azizi | About'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
